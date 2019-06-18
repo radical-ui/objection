1bc22ee389e57202bc9098976cf5399ca7ef7329
Elisarmor <ejsisgreat@gmail.com>
2019-06-18 06:53:58 -0500

    Create a test for Button

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,28 @@
 <script>
-	import { Ripple } from './components/components.module.js';
+	import { Ripple, Button } from './components/components.module.js';
 </script>
 
 <style>
-
+	.over {
+		font-size: 1rem;
+	}
 </style>
 
-<br />
-<br />
+<div class="over">
+	<br />
+	<br />
 
-<Ripple>
-	<p>Look at this! Boy is it nice!</p>
-</Ripple>
+	<Ripple>
+		<p>Look at this! Boy is it nice!</p>
+	</Ripple>
+
+	<Button>Cancel</Button>
+	<Button on:click={(e) => console.log(e)}>Save</Button>
+
+	<br />
+
+	<Button>
+		Show the
+		<i>details for Nerds</i>
+	</Button>
+</div>
