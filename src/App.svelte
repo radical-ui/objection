e8bdcaab1af84a3386f8adc7bb7a034505dd6e0b
Elisarmor <ejsisgreat@gmail.com>
2019-06-22 16:06:43 -0500

    Create the visual tests

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,11 @@
 <script>
-	import { Ripple, UIButton } from './components/components.module.js';
+	import { Ripple, UIButton, UIInput } from './components/components.module.js';
+
+	let value = 'John Smith';
+
+	function log(e) {
+		console.log(e)
+	}
 </script>
 
 <style>
@@ -63,4 +69,24 @@
 		Should go to
 		<span>https://svelte.dev</span>
 	</UIButton>
+
+	<h1>UIInput</h1>
+	<UIInput bind:value placeholder="What is your name?" maxChars={20}/>
+	<UIInput disabled={true} />
+	<UIInput type='password' error="Password is incorrect" />
+	<UIInput isFocused={true} />
+	<UIInput allowEnter={true} rows={5} compress={true} />
+	<UIInput rows={null} resize={true} />
+	<UIInput showLabel={false} />
+	<UIInput border="none" />
+	<UIInput border="full" />
+	<UIInput rows={10} error="Too many rows!" />
+
+	<UIInput
+		on:valuechanged={log}
+		on:prevaluechanged={log}
+		on:focus={log}
+		on:blur={log}
+		on:keypress={log}
+	/>
 </div>
