317f1149cdc7dc109874864219db28ddebd0b806
Elijah Mooring <ejsisgreat@gmail.com>
2019-07-19 17:07:34 -0500

    Run lint

--- src/components/switch/LabelWrapper.svelte
+++ src/components/switch/LabelWrapper.svelte
@@ -1,10 +1,10 @@
 <script>
-    export let label;
-    export let left;
+	export let label;
+	export let left;
 </script>
 
 <style>
-    div {
+	div {
 		overflow: auto;
 	}
 	.left {
@@ -23,30 +23,30 @@
 	.rounded {
 		border-radius: 3px;
 	}
-	
+
 	.switch-container {
 		max-width: 800px;
 		margin: auto;
 		padding: 5px 20px;
 		background-color: rgba(0, 0, 0, 0.1);
-		border-bottom: 0.1px solid rgba(0, 0, 0, 0.2)
+		border-bottom: 0.1px solid rgba(0, 0, 0, 0.2);
 	}
 </style>
 
 {#if label != null}
-    <div class="switch-container">
-        {#if !left}
-            <span class="left switch-text-container">{label}</span>
-            <span class="right">
-                <slot></slot>
-            </span>
-        {:else}
-            <span class="right switch-text-container">{label}</span>
-            <span class="left">
-                <slot></slot>
-            </span>
-        {/if}
-    </div>
+	<div class="switch-container">
+		{#if !left}
+			<span class="left switch-text-container">{label}</span>
+			<span class="right">
+				<slot />
+			</span>
+		{:else}
+			<span class="right switch-text-container">{label}</span>
+			<span class="left">
+				<slot />
+			</span>
+		{/if}
+	</div>
 {:else}
-    <slot></slot>
+	<slot />
 {/if}
