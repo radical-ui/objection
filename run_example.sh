095137af385a1ebf0cd711178841b8b71a8ba97e
Elijah Mooring <vehmloewff@gmail.com>
2024-07-31 09:59:45 -0700

    chore: create a script for running the example

---
+++ run_example.sh
@@ -0,0 +1,7 @@
+watchexec --restart --\
+  cargo run -p objection_cli --\
+    --runtime runtime/mod.tsx \
+    --engine rust \
+    --bindings-path example/bindings.rs \
+    --engine-url http://localhost:8000/ui run &&
+    cargo run --package example
