57e45f0b241d9c8020567ebd5dbce20537ebce5e
Elijah Mooring <vehmloewff@gmail.com>
2024-08-12 17:30:52 -0700

    fix: writer should be able to create directories when downloading files

--- cli/writer.rs
+++ cli/writer.rs
@@ -4,7 +4,7 @@ use reqwest::get;
 use sha2::{Digest, Sha256};
 use std::path::{Path, PathBuf};
 use tokio::{
-	fs::{create_dir_all, write, File},
+	fs::{create_dir_all, File},
 	io::{AsyncReadExt, AsyncWriteExt, BufReader, BufWriter},
 };
 use url::Url;
@@ -20,16 +20,13 @@ impl Writer {
 	}
 
 	pub async fn write_file(&self, path: impl AsRef<Path>, data: impl AsRef<[u8]>) -> Result<()> {
-		let joined_path = self.directory.join(path.as_ref());
-		let path_reference = joined_path.as_path();
-		let data_reference = data.as_ref();
+		let (file, joined_path) = self.create(path).await?;
+		let mut writer = BufWriter::new(file);
 
-		if let Err(_) = write(path_reference, data_reference).await {
-			create_dir_all(path_reference.parent().ok_or(anyhow!("you shouldn't be writing files to /"))?).await?;
-			write(path_reference, data_reference).await.context(
-				"failed to write, so tried to create parent directory, which succeeded. But the write directly after that, to inside the new directory, failed",
-			)?;
-		}
+		writer
+			.write_all(data.as_ref())
+			.await
+			.with_context(|| format!("failed to write {joined_path:?}"))?;
 
 		info!("Wrote {joined_path:?}");
 
@@ -38,13 +35,15 @@ impl Writer {
 
 	pub async fn get_sha256(&self, path: impl AsRef<Path>) -> Result<Vec<u8>> {
 		let mut hasher = Sha256::new();
-		let path_reference = path.as_ref();
-		let file = File::open(path_reference).await.context("failed to open {path_reference} for reading")?;
+		let joined_path = self.directory.join(path.as_ref());
+		let file = File::open(&joined_path)
+			.await
+			.with_context(|| format!("failed to open {joined_path:?} for reading"))?;
 		let mut reader = BufReader::new(file);
 		let mut buffer = [0; 1024];
 
 		loop {
-			let bytes_read = reader.read(&mut buffer).await.context("failed to read {path_reference}")?;
+			let bytes_read = reader.read(&mut buffer).await.with_context(|| format!("failed to read {joined_path:?}"))?;
 			if bytes_read == 0 {
 				break;
 			}
@@ -58,12 +57,8 @@ impl Writer {
 	/// Download `url` to `path`, returning the sha256 of the downloaded file
 	pub async fn download_file(&self, path: impl AsRef<Path>, url: &Url) -> Result<Vec<u8>> {
 		let mut hasher = Sha256::new();
-		let path_reference = path.as_ref();
-		let mut writer = BufWriter::new(
-			File::create(path_reference)
-				.await
-				.with_context(|| format!("failed to create {path_reference:?}"))?,
-		);
+		let (file, joined_path) = self.create(path).await?;
+		let mut writer = BufWriter::new(file);
 
 		if url.scheme() == "file" {
 			let mut reader = BufReader::new(File::open(url.path()).await.with_context(|| format!("failed to open {}", url.path()))?);
@@ -80,10 +75,12 @@ impl Writer {
 				writer
 					.write_all(&buffer[..bytes_read])
 					.await
-					.with_context(|| format!("failed to write to {path_reference:?}"))?;
+					.with_context(|| format!("failed to write to {joined_path:?}"))?;
 			}
+
+			info!("Copied {}", url.path());
 		} else {
-			let mut response = get(url.clone()).await.context("Failed to fetch {url}")?;
+			let mut response = get(url.clone()).await.with_context(|| format!("Failed to fetch {url}"))?;
 
 			while let Some(chunk) = response.chunk().await? {
 				hasher.update(&chunk[..]);
@@ -91,12 +88,13 @@ impl Writer {
 				writer
 					.write_all(&chunk[..])
 					.await
-					.with_context(|| format!("failed to write to {path_reference:?}"))?;
+					.with_context(|| format!("failed to write to {joined_path:?}"))?;
 			}
+
+			info!("Downloaded {url}");
 		}
 
-		info!("Downloaded {url}");
-
+		writer.flush().await?;
 		Ok(hasher.finalize().to_vec())
 	}
 
@@ -105,6 +103,26 @@ impl Writer {
 			writer: self,
 			path: path.into(),
 		}
+	}
+
+	async fn create(&self, path: impl AsRef<Path>) -> Result<(File, PathBuf)> {
+		let joined_path = self.directory.join(path.as_ref());
+
+		Ok(match File::create(&joined_path).await {
+			Ok(file) => (file, joined_path),
+			Err(_) => {
+				create_dir_all(joined_path.parent().ok_or(anyhow!("you shouldn't be writing files to /"))?).await?;
+
+				let file = File::create(&joined_path).await.with_context(|| {
+					format!(
+						"failed to write {joined_path:?}, so tried to create parent directory, which succeeded.\
+						But the write directly after that, to inside the new directory, failed"
+					)
+				})?;
+
+				(file, joined_path)
+			}
+		})
 	}
 }
 
