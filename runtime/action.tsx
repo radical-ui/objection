0968598231974fb0b330626006d74a7346ac5761
Elijah Mooring <vehmloewff@gmail.com>
2024-11-22 00:21:04 -0800

    ref: delete the old code

--- runtime/action.tsx
+++
@@ -1,9 +0,0 @@
-import { ActionKey, React, registerActionListener } from './deps.ts'
-
-export function useAction<T>(key: ActionKey<T> | null, listener: (data: T) => void) {
-	React.useEffect(() => {
-		if (!key) return
-
-		return registerActionListener(key, listener)
-	}, [key])
-}
--- runtime/component.tsx
+++
@@ -1,8 +0,0 @@
-import { Component, getComponentRenderer, React } from './deps.ts'
-
-export function ComponentRender(component: Component) {
-	const { func: Func, params } = getComponentRenderer(component)
-
-	// @ts-ignore runtime_lib is intentionally runtime-agnostic
-	return <Func {...params} />
-}
